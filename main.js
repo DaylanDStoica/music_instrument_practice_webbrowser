93011011e6906e9ffb18a406771af87422cfb8a3
DaylanDStoica <daylanstoica@gmail.com>
2025-08-09 18:44:13 -0700

    set main.js to read the homepage button press, and provide output that the button was pressed. Still need to actually create the new page.

---
+++ main.js
@@ -0,0 +1,10 @@
+//homepage for the app
+// DaylanDStoica
+// for handling musical instrument practice
+
+// handle the button presses, and create the page for the appropriate instrument practice    
+
+document.getElementById('guitarPracBtn').addEventListener('click', function() {
+    //window.location.href = 'guitar_practice.html';
+    document.getElementById('output').textContent = 'Navigating to Guitar Practice...';
+}); 
