6ba7aca74bf3c4bc3a955d9ca6cb3b121e2a6786
DaylanDStoica <daylanstoica@gmail.com>
2025-08-12 14:50:23 -0700

    set the guitar index page to go back to homepage.

--- guitar/guitar_main.js
+++ guitar/guitar_main.js
@@ -1,3 +1,6 @@
 document.getElementById('homeBtn').addEventListener('click', function() {
-    window.location.href = '../homeindex.html';
+    // document.getElementById('output').textContent = 'Navigating to Homepage...';
+
+    window.location.href = '../homeindex.html'; // Redirect to the home page
+    document.getElementById('output').textContent = 'Navigating to Home Page...';
 });
